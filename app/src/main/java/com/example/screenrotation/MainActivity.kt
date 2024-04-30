29b753451220dddd6735be0d3f5a0415fbb79793
s-iijima <s-iijima@unext.jp>
2024-04-30 15:53:05 +0900

    Add rotate button

--- app/src/main/java/com/example/screenrotation/MainActivity.kt
+++ app/src/main/java/com/example/screenrotation/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.example.screenrotation
 
+import android.content.pm.ActivityInfo
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -18,8 +22,28 @@ class MainActivity : ComponentActivity() {
         setContent {
             ScreenRotationTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Column {
+                        Greeting("Android")
+
+                        Button(onClick = {
+                            val orientation = resources.configuration.orientation
+
+                            val targetRotation =
+                                if (orientation == Configuration.ORIENTATION_PORTRAIT) {
+                                    ActivityInfo.SCREEN_ORIENTATION_SENSOR_LANDSCAPE
+                                } else {
+                                    ActivityInfo.SCREEN_ORIENTATION_SENSOR_PORTRAIT
+                                }
+                            
+                            requestedOrientation = targetRotation
+                        }) {
+                            Text("Rotate")
+                        }
+                    }
                 }
             }
         }
